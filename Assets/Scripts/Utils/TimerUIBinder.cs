c16f41559d441df421f77f7ab71a737d0e55e44b
LizaChepurko <lizachep@gmail.com>
2026-03-29 21:25:59 +0300

    commit

--- Assets/Scripts/Utils/TimerUIBinder.cs
+++ Assets/Scripts/Utils/TimerUIBinder.cs
@@ -1,14 +1,24 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class TimerUIBinder : MonoBehaviour
 {
     [SerializeField] private Text clockText;
     [SerializeField] private Image background;
 
-    void OnEnable()
+    private IEnumerator Start()
     {
+        yield return null;
+
         if (Timer.Instance != null)
+        {
             Timer.Instance.BindUI(clockText, background);
+            Debug.Log("[TimerUIBinder] Successfully bound UI in the new scene.");
+        }
+        else
+        {
+            Debug.LogWarning("[TimerUIBinder] Timer.Instance is null! Is the Timer persistent?");
+        }
     }
 }
